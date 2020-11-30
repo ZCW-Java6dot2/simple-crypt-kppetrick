e009b889d62c32e10c0b1fae953068c9e09edab3
kppetrick <kppetrick@gmail.com>
2020-11-30 12:38:39 -0500

    done

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -2,31 +2,65 @@ import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
+
+    private Character cs;
+    private Character cf;
 
     ROT13(Character cs, Character cf) {
+        this.cs = cs;
+        this.cf = cf;
     }
 
     ROT13() {
+        cs = 'a';
+        cf = 'n';
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        String answer = "";
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if ( ch >= 'a' && ch <= 'm') {
+                ch += 13;
+            } else if ( ch >= 'A' && ch <= 'M') {
+                ch += 13;
+            } else if ( ch >= 'n' && ch <= 'z') {
+                ch -= 13;
+            } else if ( ch >= 'N' && ch <= 'Z') {
+                ch -= 13;
+            }
+            answer = answer + ch;
+        }
+        return answer;
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
-
-        return "";
+        int startIndex = 0;
+        String answer = "";
+        char[] stringS = s.toCharArray();
+        for (int i = 0; i < stringS.length; i++) {
+            if (stringS[i] == c) {
+                startIndex = i;
+            }
+        }
+        for (int i = startIndex; i < stringS.length; i++) {
+            answer = answer + stringS[i];
+        }
+        for (int i = 0; i < startIndex; i++) {
+            answer = answer + stringS[i];
+        }
+        return answer;
     }
-
 }
+
+
